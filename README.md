1e399f31f63fdb9b6d046722d1b3bdc3a803a897
Nathen Harvey <nharvey@chef.io>
2016-08-29 23:56:39 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# my_hostname
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: my_hostname
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
